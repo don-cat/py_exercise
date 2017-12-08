6280f35e4fd3cee19e5c860e70e133144952b18e
don-cat <mamayanyu@hotmail.com>
2017-12-09 00:21:49 +0800

    exercise 14

--- blog/test/cainiao14.py
+++ blog/test/cainiao14.py
@@ -1,2 +1,10 @@
 #将一个正整数分解质因数。例如：输入90,打印出90=2*3*3*5。
-def exercise14():
+def exercise14(num):
+    if num!=1:
+        for i in range(2,int(num+1)):
+            if num%i==0:
+                print(i)
+                exercise14(int(num/i))
+                return
+
+exercise14(90)
