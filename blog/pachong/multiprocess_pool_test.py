4d817e99d559a0fb73ef63dd1a8efd65a7996e56
don-cat <mamayanyu@hotmail.com>
2018-01-01 23:52:47 +0800

    multiprocess exercise

--- blog/pachong/multiprocess_pool_test.py
+++ blog/pachong/multiprocess_pool_test.py
@@ -1,14 +1,19 @@
-import os
+import os,time
 from multiprocessing import Pool
 def multiprocessing_exercise(i):
     print("this is process:%s,num:%s"% (os.getpid(),i))
     print("process end %s,num:%s"% (os.getpid(),i))
 
 if __name__ == "__main__":
-    pool = Pool(processes=5)
+    pool = Pool(5)
     print("进程初始化")
-    for i in range(50):
-        pool.apply_async(multiprocessing_exercise,args=(i))
+    for i in range(6):
+        try:
+            pool.apply_async(multiprocessing_exercise,args=(i,))
+        except Exception as e:
+            print(e)
+        # pool.apply(multiprocessing_exercise,args=(i))
     pool.close()
+    time.sleep(10)
     pool.join()
     print("process end %s"% os.getpid())
